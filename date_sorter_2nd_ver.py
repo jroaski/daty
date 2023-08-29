121f40b13d6c94306036a9ca6136a95660a5fd88
Jakub Rozanski <jroaski@gmail.com>
2023-08-29 18:34:01 +0200

    functions added

--- date_sorter_2nd_ver.py
+++ date_sorter_2nd_ver.py
@@ -1,22 +1,31 @@
 import datefinder
 
-with open(r"C:\Users\Jacob\Desktop\daty.txt", 'r') as f:
-    full_text = f.read()
 
-text_dict = {}
-previous_key = None
-first_date_beginning = None
-text_len = len(full_text)
-dates = datefinder.find_dates(full_text, index=True, strict=True)
-for date in dates:
-    new_key = (date[0], date[1][0])
-    text_dict[new_key] = text_len
-    if previous_key:
-        text_dict[previous_key] = new_key[1]
-    else:
-        first_date_beginning = new_key[1]
+def read_text_file(file_path):
+    with open(file_path, 'r') as f:
+        return f.read()
 
-    previous_key = new_key
 
-unmapped_text = full_text[0:first_date_beginning]
-mapped_text = '\n\n'.join([full_text[key[1]:value] for key, value in sorted(text_dict.items())])
+def extract_dates_and_text(full_text):
+    text_dict = {}
+    previous_key = None
+    first_date_beginning = None
+    text_len = len(full_text)
+    dates = datefinder.find_dates(full_text, index=True, strict=True)
+
+    for date in dates:
+        new_key = (date[0], date[1][0])
+        text_dict[new_key] = text_len
+        if previous_key:
+            text_dict[previous_key] = new_key[1]
+        else:
+            first_date_beginning = new_key[1]
+
+        previous_key = new_key
+
+    unmapped_text = full_text[0:first_date_beginning]
+    mapped_text = '\n\n'.join([full_text[key[1]:value] for key, value in sorted(text_dict.items())])
+
+    return unmapped_text, mapped_text
+
+
