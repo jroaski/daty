b0d49ccdb12352ea0bcd827c96157dfc4f548bc2
Jakub Rozanski <jroaski@gmail.com>
2023-08-31 13:46:33 +0200

    More tests

--- testy_sortu.py
+++ testy_sortu.py
@@ -15,7 +15,26 @@ class FunctionsTests(unittest.TestCase):
         result = get_date(line)
         self.assertIsNone(result)
 
+    def test_key_valid_date(self):
+        group = ["10.08.2023 Some text"]
+        result = sort_key(group)
+        expected = datetime(2023, 8, 10)
+        self.assertEqual(result, expected)
 
+    def test_lines_by_dates(self):
+        lines = [
+            "01.08.2023 radom shrandom words",
+            "Text 2",
+            "02.08.2023 Text 3",
+            "03.08.2023 Text 4",
+        ]
+        result = group_lines_by_dates(lines)
+        expected = [
+            ["01.08.2023 radom shrandom words", "Text 2"],
+            ["02.08.2023 Text 3"],
+            ["03.08.2023 Text 4"],
+        ]
+        self.assertEqual(result, expected)
 
 
 if __name__ == '__main__':
